001f9434b3b8e162026429f31cd72fb90664dd3a
friday04326-dark <friday04326@gmail.com>
2026-05-29 00:58:46 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Dashboard from './app/components/Dashboard'
-import './index.css' // Keep your global styles
+import './app/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
