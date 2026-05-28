7eaa5cb9308f7a80fab1f48efdfc622d9228e2cf
friday04326-dark <friday04326@gmail.com>
2026-05-29 01:26:07 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Dashboard from './app/components/Dashboard';
+import App from './app/components/App';
 import './app/globals.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>
 );
